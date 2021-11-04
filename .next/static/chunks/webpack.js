ac9442fa52b63e8121a65c009b1b224efff711e0
gollavilli ganesh kumar <ganeshgollavilli@gmail.com>
2021-11-04 21:49:54 +0530

    comit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "77bcd8c7d57ae8bb"; }
+/******/ 		__webpack_require__.h = function() { return "0586655d47904100"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
