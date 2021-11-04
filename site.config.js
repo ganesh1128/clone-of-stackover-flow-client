9e7297c016153810f112f53b5220a6d0bfe012bf
gollavilli ganesh kumar <ganeshgollavilli@gmail.com>
2021-11-04 17:24:52 +0530

    commited

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Stackoverflow',
   description: 'Where Developers Learn & Share',
-  siteUrl: 'https://clone-of-stackoverflow.vercel.app',
+  siteUrl: '',
   googleAnalytic: 'UA-149699646-1',
   author: {
     email: 'ganeshgollavilli@gmail.com',
